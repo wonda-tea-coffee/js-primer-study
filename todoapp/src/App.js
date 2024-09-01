7b3cbcda670ce8a2f98dd797e51cdf0963c3c48d
Rikita Ishikawa <lagrange.resolvent@gmail.com>
2024-09-01 09:19:23 +0900

    update delete

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -10,16 +10,39 @@ export class App {
     const inputElement = document.querySelector("#js-form-input");
     const containerElement = document.querySelector("#js-todo-list");
     const todoItemCountElement = document.querySelector("#js-todo-count");
+
     this.#todoListModel.onChange(() => {
       const todoListElement = element`<ul></ul>`;
       const todoItems = this.#todoListModel.getTodoItems();
       todoItems.forEach(item => {
-        const todoItemElement = element`<li>${item.title}</li>`;
+        const todoItemElement = item.completed
+          ? element`<li><input type="checkbox" class="checkbox" checked>
+              <s>${item.title}</s>
+              <button class="delete">x</button>
+          </li>`
+          : element`<li><input type="checkbox" class="checkbox">
+            ${item.title}
+            <button class="delete">x</button>
+          </li>`;
+        const inputCheckboxElement = todoItemElement.querySelector(".checkbox");
+        inputCheckboxElement.addEventListener("change", () => {
+          this.#todoListModel.updateTodo({
+            id: item.id,
+            completed: !item.completed
+          })
+        });
+        const deleteButtonElement = todoItemElement.querySelector(".delete");
+        deleteButtonElement.addEventListener("click", () => {
+          this.#todoListModel.deleteTodo({
+            id: item.id
+          });
+        });
         todoListElement.appendChild(todoItemElement);
       });
       render(todoListElement, containerElement);
       todoItemCountElement.textContent = `Todoアイテム数: ${this.#todoListModel.getTotalCount()}`;
     })
+
     formElement.addEventListener("submit", (event) => {
       event.preventDefault();
       this.#todoListModel.addTodo(new TodoItemModel({
